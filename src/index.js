d29dc6752e3f509f97407c458b3af14a2cd9582d
jiwen xin <kevinxin@scripps.edu>
2021-03-12 10:55:16 -0800

    style: :art: style code

--- src/index.js
+++ src/index.js
@@ -13,94 +13,95 @@ const debug = require('debug')('biothings-explorer-trapi:main');
 exports.InvalidQueryGraphError = InvalidQueryGraphError;
 
 exports.TRAPIQueryHandler = class TRAPIQueryHandler {
-    constructor(options = {}, smartAPIPath = undefined) {
-        this.logs = [];
-        this.options = options;
-        this.resolveOutputIDs = (typeof this.options.enableIDResolution === "undefined") ? true : this.options.enableIDResolution;
-        this.path = smartAPIPath || path.resolve(__dirname, './smartapi_specs.json');
-    }
+  constructor(options = {}, smartAPIPath = undefined) {
+    this.logs = [];
+    this.options = options;
+    this.resolveOutputIDs =
+      typeof this.options.enableIDResolution === 'undefined' ? true : this.options.enableIDResolution;
+    this.path = smartAPIPath || path.resolve(__dirname, './smartapi_specs.json');
+  }
 
-    _loadMetaKG() {
-        const kg = new meta_kg.default(this.path);
-        debug(`Query options are: ${JSON.stringify(this.options)}`);
-        debug(`SmartAPI Specs read from path: ${this.path}`);
-        kg.constructMetaKGSync(this.options);
-        return kg;
-    }
+  _loadMetaKG() {
+    const kg = new meta_kg.default(this.path);
+    debug(`Query options are: ${JSON.stringify(this.options)}`);
+    debug(`SmartAPI Specs read from path: ${this.path}`);
+    kg.constructMetaKGSync(this.options);
+    return kg;
+  }
 
-    getResponse() {
-        return {
-            message: {
-                query_graph: this.queryGraph,
-                knowledge_graph: this.knowledgeGraph.kg,
-                results: this.queryResults.getResults(),
-            },
-            logs: this.logs,
-        };
-    }
+  getResponse() {
+    return {
+      message: {
+        query_graph: this.queryGraph,
+        knowledge_graph: this.knowledgeGraph.kg,
+        results: this.queryResults.getResults(),
+      },
+      logs: this.logs,
+    };
+  }
 
-    /**
-     * Set TRAPI Query Graph
-     * @param {object} queryGraph - TRAPI Query Graph Object
-     */
-    setQueryGraph(queryGraph) {
-        this.queryGraph = queryGraph;
-    }
+  /**
+   * Set TRAPI Query Graph
+   * @param {object} queryGraph - TRAPI Query Graph Object
+   */
+  setQueryGraph(queryGraph) {
+    this.queryGraph = queryGraph;
+  }
 
-    _initializeResponse() {
-        this.knowledgeGraph = new KnowledgeGraph();
-        this.queryResults = new QueryResults();
-    }
+  _initializeResponse() {
+    this.knowledgeGraph = new KnowledgeGraph();
+    this.queryResults = new QueryResults();
+  }
 
-    /**
-     * @private
-     * @param {object} queryGraph - TRAPI Query Graph Object
-     */
-    _processQueryGraph(queryGraph) {
-        try {
-            let queryGraphHandler = new QueryGraph(queryGraph);
-            let res = queryGraphHandler.createQueryPaths();
-            this.logs = [...this.logs, ...queryGraphHandler.logs];
-            return res;
-        } catch (err) {
-            if (err instanceof InvalidQueryGraphError) {
-                throw err;
-            } else {
-                throw new InvalidQueryGraphError();
-            }
-        }
+  /**
+   * @private
+   * @param {object} queryGraph - TRAPI Query Graph Object
+   */
+  _processQueryGraph(queryGraph) {
+    try {
+      let queryGraphHandler = new QueryGraph(queryGraph);
+      let res = queryGraphHandler.createQueryPaths();
+      this.logs = [...this.logs, ...queryGraphHandler.logs];
+      return res;
+    } catch (err) {
+      if (err instanceof InvalidQueryGraphError) {
+        throw err;
+      } else {
+        throw new InvalidQueryGraphError();
+      }
     }
+  }
 
-    _createBatchEdgeQueryHandlers(queryPaths, kg) {
-        let handlers = {};
-        for (const index in queryPaths) {
-            handlers[index] = new BatchEdgeQueryHandler(kg, this.resolveOutputIDs);
-            handlers[index].setEdges(queryPaths[index]);
-            handlers[index].subscribe(this.knowledgeGraph);
-            handlers[index].subscribe(this.queryResults);
-        }
-        return handlers;
+  _createBatchEdgeQueryHandlers(queryPaths, kg) {
+    let handlers = {};
+    for (const index in queryPaths) {
+      handlers[index] = new BatchEdgeQueryHandler(kg, this.resolveOutputIDs);
+      handlers[index].setEdges(queryPaths[index]);
+      handlers[index].subscribe(this.knowledgeGraph);
+      handlers[index].subscribe(this.queryResults);
     }
+    return handlers;
+  }
 
-    async query() {
-        this._initializeResponse();
-        debug('start to load metakg.');
-        const kg = this._loadMetaKG(this.smartapiID, this.team);
-        debug('metakg successfully loaded');
-        let queryPaths = this._processQueryGraph(this.queryGraph);
-        debug(`query paths constructed: ${queryPaths}`);
-        const handlers = this._createBatchEdgeQueryHandlers(queryPaths, kg);
-        debug(`Query depth is ${Object.keys(handlers).length}`);
-        for (let i = 0; i < Object.keys(handlers).length; i++) {
-            debug(`Start to query depth ${i + 1}`);
-            let res = await handlers[i].query(handlers[i].qEdges);
-            debug(`Query for depth ${i + 1} completes.`);
-            this.logs = [...this.logs, ...handlers[i].logs];
-            if (res.length === 0) {
-                return;
-            }
-            handlers[i].notify(res);
-            debug(`Updated TRAPI knowledge graph using query results for depth ${i + 1}`);
-        }
+  async query() {
+    this._initializeResponse();
+    debug('start to load metakg.');
+    const kg = this._loadMetaKG(this.smartapiID, this.team);
+    debug('metakg successfully loaded');
+    let queryPaths = this._processQueryGraph(this.queryGraph);
+    debug(`query paths constructed: ${queryPaths}`);
+    const handlers = this._createBatchEdgeQueryHandlers(queryPaths, kg);
+    debug(`Query depth is ${Object.keys(handlers).length}`);
+    for (let i = 0; i < Object.keys(handlers).length; i++) {
+      debug(`Start to query depth ${i + 1}`);
+      let res = await handlers[i].query(handlers[i].qEdges);
+      debug(`Query for depth ${i + 1} completes.`);
+      this.logs = [...this.logs, ...handlers[i].logs];
+      if (res.length === 0) {
+        return;
+      }
+      handlers[i].notify(res);
+      debug(`Updated TRAPI knowledge graph using query results for depth ${i + 1}`);
     }
+  }
 };
--- src/query_edge.js
+++ src/query_edge.js
@@ -3,99 +3,99 @@ const debug = require('debug')('biothings-explorer-trapi:QEdge');
 const reverse = require('./reverse');
 
 module.exports = class QEdge {
-    /**
-     *
-     * @param {string} id - QEdge ID
-     * @param {object} info - QEdge info, e.g. subject, object, predicate
-     */
-    constructor(id, info) {
-        this.id = id;
-        this.predicate = info.predicate;
-        this.subject = info.subject;
-        this.object = info.object;
-    }
+  /**
+   *
+   * @param {string} id - QEdge ID
+   * @param {object} info - QEdge info, e.g. subject, object, predicate
+   */
+  constructor(id, info) {
+    this.id = id;
+    this.predicate = info.predicate;
+    this.subject = info.subject;
+    this.object = info.object;
+  }
 
-    getID() {
-        return this.id;
-    }
+  getID() {
+    return this.id;
+  }
 
-    setID(id) {
-        this.id = id;
-    }
+  setID(id) {
+    this.id = id;
+  }
 
-    getHashedEdgeRepresentation() {
-        const toBeHashed =
-            this.subject.getCategories() + this.predicate + this.object.getCategories() + this.getInputPrimaryIDs();
-        return new helper()._generateHash(toBeHashed);
-    }
+  getHashedEdgeRepresentation() {
+    const toBeHashed =
+      this.subject.getCategories() + this.predicate + this.object.getCategories() + this.getInputPrimaryIDs();
+    return new helper()._generateHash(toBeHashed);
+  }
 
-    getQueryPredicate() {
-        if (this.predicate && this.predicate.startsWith('biolink:')) {
-            return this.predicate.slice(8);
-        }
-        return this.predicate;
+  getQueryPredicate() {
+    if (this.predicate && this.predicate.startsWith('biolink:')) {
+      return this.predicate.slice(8);
     }
+    return this.predicate;
+  }
 
-    getPredicate() {
-        let predicate = this.predicate;
-        if (this.predicate && this.predicate.startsWith('biolink:')) {
-            predicate = this.predicate.slice(8);
-        }
-        if (this.isReversed()) {
-            return reverse.reverse(predicate);
-        }
-        return predicate;
+  getPredicate() {
+    let predicate = this.predicate;
+    if (this.predicate && this.predicate.startsWith('biolink:')) {
+      predicate = this.predicate.slice(8);
     }
+    if (this.isReversed()) {
+      return reverse.reverse(predicate);
+    }
+    return predicate;
+  }
 
-    getSubject() {
-        if (this.isReversed()) {
-            return this.object;
-        }
-        return this.subject;
+  getSubject() {
+    if (this.isReversed()) {
+      return this.object;
     }
+    return this.subject;
+  }
 
-    getObject() {
-        if (this.isReversed()) {
-            return this.subject;
-        }
-        return this.object;
+  getObject() {
+    if (this.isReversed()) {
+      return this.subject;
     }
+    return this.object;
+  }
 
-    isReversed() {
-        return this.subject.getCurie() === undefined && this.object.getCurie() !== undefined;
-    }
+  isReversed() {
+    return this.subject.getCurie() === undefined && this.object.getCurie() !== undefined;
+  }
 
-    getInputCurie() {
-        let curie = this.subject.getCurie() || this.object.getCurie();
-        if (Array.isArray(curie)) {
-            return curie;
-        }
-        return [curie];
+  getInputCurie() {
+    let curie = this.subject.getCurie() || this.object.getCurie();
+    if (Array.isArray(curie)) {
+      return curie;
     }
+    return [curie];
+  }
 
-    getInputPrimaryIDs() {
-        return this.isReversed() ? this.object.getPrimaryIDs() : this.subject.getPrimaryIDs();
-    }
+  getInputPrimaryIDs() {
+    return this.isReversed() ? this.object.getPrimaryIDs() : this.subject.getPrimaryIDs();
+  }
 
-    getInputNode() {
-        return this.isReversed() ? this.object : this.subject;
-    }
+  getInputNode() {
+    return this.isReversed() ? this.object : this.subject;
+  }
 
-    getOutputNode() {
-        return this.isReversed() ? this.subject : this.object;
-    }
+  getOutputNode() {
+    return this.isReversed() ? this.subject : this.object;
+  }
 
-    hasInputResolved() {
-        if (this.isReversed()) {
-            return this.object.hasEquivalentIDs();
-        }
-        return this.subject.hasEquivalentIDs();
+  hasInputResolved() {
+    if (this.isReversed()) {
+      return this.object.hasEquivalentIDs();
     }
+    return this.subject.hasEquivalentIDs();
+  }
 
-    hasInput() {
-        if (this.isReversed()) {
-            return this.object.hasInput();
-        }
-        return this.subject.hasInput();
+  hasInput() {
+    if (this.isReversed()) {
+      return this.object.hasInput();
     }
+    return this.subject.hasInput();
+  }
 };
