bc451a26578c7715926e953f1fc7a582c6e30a39
jiwen xin <kevinxin@scripps.edu>
2021-03-15 09:59:58 -0700

    test: :white_check_mark: add test for QueryNode class

--- __test__/integration/QueryNode.test.js
+++ __test__/integration/QueryNode.test.js
@@ -38,4 +38,98 @@ describe("Testing QueryNode Module", () => {
             expect(res).toBeFalsy();
         })
     })
+
+    describe("Test getEntities", () => {
+        test("If equivalent ids are empty, should return an empty array", () => {
+            const gene_node = new QNode("n1", { category: "Gene" });
+            gene_node.equivalentIDs = {};
+            expect(gene_node.getEntities()).toEqual([]);
+        })
+
+        test("If equivalent ids are not empty, should return an array of bioentities", () => {
+            const gene_node = new QNode("n1", { category: "Gene" });
+            gene_node.equivalentIDs = {
+                "A": [
+                    {
+                        "a": "b"
+                    },
+                    {
+                        "c": "d"
+                    }
+                ],
+                "B": [
+                    {
+                        "e": "f"
+                    }
+                ]
+            };
+            expect(gene_node.getEntities()).toEqual([
+                {
+                    "a": "b"
+                },
+                {
+                    "c": "d"
+                },
+                {
+                    "e": "f"
+                }
+            ]);
+        })
+    })
+
+    describe("Test getPrimaryIDs", () => {
+        test("If equivalent ids are empty, should return an empty array", () => {
+            const gene_node = new QNode("n1", { category: "Gene" });
+            gene_node.equivalentIDs = {};
+            expect(gene_node.getPrimaryIDs()).toEqual([]);
+        })
+
+        test("If equivalent ids are not empty, should return an array of primaryIDs", () => {
+            const gene_node = new QNode("n1", { category: "Gene" });
+            gene_node.equivalentIDs = {
+                "A": [
+                    {
+                        "primaryID": "b"
+                    },
+                    {
+                        "primaryID": "c"
+                    }
+                ],
+                "B": [
+                    {
+                        "primaryID": "d"
+                    }
+                ]
+            };
+            expect(gene_node.getPrimaryIDs()).toEqual(["b", "c", "d"])
+        })
+    })
+
+    describe("Test updateEquivalentIDs", () => {
+        test("If equivalent ids does not exist, should set it with the input", () => {
+            const gene_node = new QNode("n1", { category: "Gene" });
+            gene_node.updateEquivalentIDs({ "a": "b" })
+            expect(gene_node.equivalentIDs).toEqual({ "a": "b" });
+        })
+
+        test("If equivalent ids are not empty, should update the equivalent ids", () => {
+            const gene_node = new QNode("n1", { category: "Gene" });
+            gene_node.equivalentIDs = {
+                "A": [
+                    {
+                        "primaryID": "b"
+                    },
+                    {
+                        "primaryID": "c"
+                    }
+                ],
+                "B": [
+                    {
+                        "primaryID": "d"
+                    }
+                ]
+            };
+            expect(gene_node.getPrimaryIDs()).toEqual(["b", "c", "d"])
+        })
+    })
 })
