ce7e94d4cba51e5fcc83673d6269e098808e5a96
jiwen xin <kevinxin@scripps.edu>
2021-03-15 09:59:49 -0700

    refactor: :fire: remove unused method

--- src/query_node.js
+++ src/query_node.js
@@ -2,74 +2,70 @@ const _ = require('lodash');
 const utils = require('./utils');
 
 module.exports = class QNode {
-  /**
-   *
-   * @param {string} id - QNode ID
-   * @param {object} info - Qnode info, e.g. curie, category
-   */
-  constructor(id, info) {
-    this.id = id;
-    this.category = info.category || 'NamedThing';
-    this.curie = info.id;
-  }
-
-  getID() {
-    return this.id;
-  }
-
-  getCategory() {
-    return utils.removeBioLinkPrefix(this.category);
-  }
-
-  getCurie() {
-    return this.curie;
-  }
-
-  getEquivalentIDs() {
-    return this.equivalentIDs;
-  }
-
-  getCategories() {
-    if (this.hasEquivalentIDs() === false) {
-      const categories = utils.toArray(this.category);
-      return utils.getUnique(categories.map((category) => utils.removeBioLinkPrefix(category)));
+    /**
+     *
+     * @param {string} id - QNode ID
+     * @param {object} info - Qnode info, e.g. curie, category
+     */
+    constructor(id, info) {
+        this.id = id;
+        this.category = info.category || 'NamedThing';
+        this.curie = info.id;
     }
-    let categories = [];
-    Object.values(this.equivalentIDs).map((entities) => {
-      entities.map((entity) => {
-        categories = [...categories, ...entity.semanticTypes];
-      });
-    });
-    return utils.getUnique(categories);
-  }
 
-  getEntities() {
-    return Object.values(this.equivalentIDs).reduce((res, entities) => {
-      return [...res, ...entities];
-    }, []);
-  }
-
-  getPrimaryIDs() {
-    return this.getEntities().map((entity) => entity.primaryID);
-  }
-
-  setEquivalentIDs(equivalentIDs) {
-    this.equivalentIDs = equivalentIDs;
-  }
-
-  updateEquivalentIDs(equivalentIDs) {
-    if (this.equivalentIDs === undefined) {
-      this.equivalentIDs = equivalentIDs;
-    } else {
-      this.equivalentIDs = { ...this.equivalentIDs, ...equivalentIDs };
+    getID() {
+        return this.id;
     }
-  }
 
-  hasInput() {
-    return !(typeof this.curie === 'undefined');
-  }
+    getCurie() {
+        return this.curie;
+    }
 
-  hasEquivalentIDs() {
-    return !(typeof this.equivalentIDs === 'undefined');
-  }
+    getEquivalentIDs() {
+        return this.equivalentIDs;
+    }
+
+    getCategories() {
+        if (this.hasEquivalentIDs() === false) {
+            const categories = utils.toArray(this.category);
+            return utils.getUnique(categories.map((category) => utils.removeBioLinkPrefix(category)));
+        }
+        let categories = [];
+        Object.values(this.equivalentIDs).map((entities) => {
+            entities.map((entity) => {
+                categories = [...categories, ...entity.semanticTypes];
+            });
+        });
+        return utils.getUnique(categories);
+    }
+
+    getEntities() {
+        return Object.values(this.equivalentIDs).reduce((res, entities) => {
+            return [...res, ...entities];
+        }, []);
+    }
+
+    getPrimaryIDs() {
+        return this.getEntities().map((entity) => entity.primaryID);
+    }
+
+    setEquivalentIDs(equivalentIDs) {
+        this.equivalentIDs = equivalentIDs;
+    }
+
+    updateEquivalentIDs(equivalentIDs) {
+        if (this.equivalentIDs === undefined) {
+            this.equivalentIDs = equivalentIDs;
+        } else {
+            this.equivalentIDs = { ...this.equivalentIDs, ...equivalentIDs };
+        }
+    }
+
+    hasInput() {
+        return !(typeof this.curie === 'undefined');
+    }
+
+    hasEquivalentIDs() {
+        return !(typeof this.equivalentIDs === 'undefined');
+    }
 };
