5657460b80f81e4402fd8f7747a202fd81a89eaa
jiwen xin <kevinxin@scripps.edu>
2021-03-16 09:39:00 -0700

    style: :art: style code

--- src/redis-client.js
+++ src/redis-client.js
@@ -6,22 +6,22 @@ let client;
 const enableRedis = !(process.env.REDIS_HOST === undefined) && !(process.env.REDIS_PORT === undefined);
 
 if (enableRedis === true) {
-    client = redis.createClient({
-        port: process.env.REDIS_PORT,
-        host: process.env.REDIS_HOST,
-    });
+  client = redis.createClient({
+    port: process.env.REDIS_PORT,
+    host: process.env.REDIS_HOST,
+  });
 }
 
 const redisClient =
-    enableRedis === true
-        ? {
-            ...client,
-            getAsync: promisify(client.get).bind(client),
-            setAsync: promisify(client.set).bind(client)
-            // hmsetAsync: promisify(client.hmset).bind(client),
-            // keysAsync: promisify(client.keys).bind(client),
-            // existsAsync: promisify(client.exists).bind(client),
-        }
-        : {};
+  enableRedis === true
+    ? {
+        ...client,
+        getAsync: promisify(client.get).bind(client),
+        setAsync: promisify(client.set).bind(client),
+        // hmsetAsync: promisify(client.hmset).bind(client),
+        // keysAsync: promisify(client.keys).bind(client),
+        // existsAsync: promisify(client.exists).bind(client),
+      }
+    : {};
 
 module.exports = redisClient;
