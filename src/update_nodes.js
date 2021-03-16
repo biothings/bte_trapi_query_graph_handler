195b40b387395571f334fdd5dbd5480d8ccc4665
jiwen xin <kevinxin@scripps.edu>
2021-03-16 09:43:54 -0700

    fix: :bug: fix issue regarding output id not correctly updated

--- src/update_nodes.js
+++ src/update_nodes.js
@@ -3,102 +3,102 @@ const _ = require('lodash');
 const debug = require('debug')('biothings-explorer-trapi:nodeUpdateHandler');
 
 module.exports = class NodesUpdateHandler {
-  constructor(qEdges) {
-    this.qEdges = qEdges;
-  }
+    constructor(qEdges) {
+        this.qEdges = qEdges;
+    }
 
-  /**
-   * @private
-   * s
-   */
-  _getCuries(qEdges) {
-    let curies = {};
-    qEdges.map((edge) => {
-      if (edge.hasInputResolved()) {
-        return;
-      }
-      if (edge.hasInput()) {
-        const inputCategories = edge.getSubject().getCategories();
-        inputCategories.map((category) => {
-          if (!(category in curies)) {
-            curies[category] = [];
-          }
-          curies[category] = [...curies[category], ...edge.getInputCurie()];
+    /**
+     * @private
+     * s
+     */
+    _getCuries(qEdges) {
+        let curies = {};
+        qEdges.map((edge) => {
+            if (edge.hasInputResolved()) {
+                return;
+            }
+            if (edge.hasInput()) {
+                const inputCategories = edge.getSubject().getCategories();
+                inputCategories.map((category) => {
+                    if (!(category in curies)) {
+                        curies[category] = [];
+                    }
+                    curies[category] = [...curies[category], ...edge.getInputCurie()];
+                });
+            }
         });
-      }
-    });
-    return curies;
-  }
-
-  /**
-   * Resolve input ids
-   * @param {object} curies - each key represents the category, e.g. gene, value is an array of curies.
-   */
-  async _getEquivalentIDs(curies) {
-    const resolver = new id_resolver.Resolver('biolink');
-    const equivalentIDs = await resolver.resolve(curies);
-    return equivalentIDs;
-  }
-
-  async setEquivalentIDs(qEdges) {
-    const curies = this._getCuries(this.qEdges);
-    if (curies.length === 0) {
-      return;
+        return curies;
     }
-    debug(`curies: ${JSON.stringify(curies)}`);
-    const equivalentIDs = await this._getEquivalentIDs(curies);
-    qEdges.map((edge) => {
-      debug(`Edge input curie is ${edge.getInputCurie()}`);
-      let edgeEquivalentIDs = Object.keys(equivalentIDs)
-        .filter((key) => edge.getInputCurie().includes(key))
-        .reduce((res, key) => {
-          return { ...res, [key]: equivalentIDs[key] };
-        }, {});
-      debug(`Edge Equivalent IDs are: ${JSON.stringify(edgeEquivalentIDs)}`);
-      if (Object.keys(edgeEquivalentIDs).length > 0) {
-        edge.getSubject().setEquivalentIDs(edgeEquivalentIDs);
-      }
-    });
-    return;
-  }
 
-  _createEquivalentIDsObject(record) {
-    if (record.$output.obj !== undefined) {
-      return {
-        [record.$output.obj.primaryID]: record.$output.obj,
-      };
-    } else {
-      return;
+    /**
+     * Resolve input ids
+     * @param {object} curies - each key represents the category, e.g. gene, value is an array of curies.
+     */
+    async _getEquivalentIDs(curies) {
+        const resolver = new id_resolver.Resolver('biolink');
+        const equivalentIDs = await resolver.resolve(curies);
+        return equivalentIDs;
     }
-  }
 
-  /**
-   * Update nodes with equivalent ids based on query response.
-   * @param {object} queryResult - query response
-   */
-  update(queryResult) {
-    debug('update nodes now!');
-    const node_dict = {};
-    const id_dict = {};
-    // queryResult.map(record => {
-    //     record.$edge_metadata.trapi_qEdge_obj.getOutputNode().updateEquivalentIDs(
-    //         this._createEquivalentIDsObject(record)
-    //     );
-    // })
-    debug(`queryResult, ${queryResult}`);
-    queryResult.map((record) => {
-      const nodeID = record.$edge_metadata.trapi_qEdge_obj.getOutputNode().getID();
-      if (!(nodeID in id_dict)) {
-        id_dict[nodeID] = {};
-        node_dict[nodeID] = record.$edge_metadata.trapi_qEdge_obj.getOutputNode();
-      }
-      debug(`output obj record: ${JSON.stringify(record.$output.obj)}`);
-      if (!(record.$output.obj.primaryID in id_dict[nodeID])) {
-        id_dict[nodeID][record.$output.obj.primaryID] = record.$output.obj;
-      }
-    });
-    for (const nodeID in id_dict) {
-      node_dict[nodeID].updateEquivalentIDs(id_dict[nodeID]);
+    async setEquivalentIDs(qEdges) {
+        const curies = this._getCuries(this.qEdges);
+        if (curies.length === 0) {
+            return;
+        }
+        debug(`curies: ${JSON.stringify(curies)}`);
+        const equivalentIDs = await this._getEquivalentIDs(curies);
+        qEdges.map((edge) => {
+            debug(`Edge input curie is ${edge.getInputCurie()}`);
+            let edgeEquivalentIDs = Object.keys(equivalentIDs)
+                .filter((key) => edge.getInputCurie().includes(key))
+                .reduce((res, key) => {
+                    return { ...res, [key]: equivalentIDs[key] };
+                }, {});
+            debug(`Edge Equivalent IDs are: ${JSON.stringify(edgeEquivalentIDs)}`);
+            if (Object.keys(edgeEquivalentIDs).length > 0) {
+                edge.getSubject().setEquivalentIDs(edgeEquivalentIDs);
+            }
+        });
+        return;
     }
-  }
+
+    _createEquivalentIDsObject(record) {
+        if (record.$output.obj !== undefined) {
+            return {
+                [record.$output.obj.primaryID]: record.$output.obj,
+            };
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Update nodes with equivalent ids based on query response.
+     * @param {object} queryResult - query response
+     */
+    update(queryResult) {
+        debug('update nodes now!');
+        const node_dict = {};
+        const id_dict = {};
+        // queryResult.map(record => {
+        //     record.$edge_metadata.trapi_qEdge_obj.getOutputNode().updateEquivalentIDs(
+        //         this._createEquivalentIDsObject(record)
+        //     );
+        // })
+        debug(`queryResult, ${queryResult}`);
+        queryResult.map((record) => {
+            const nodeID = record.$edge_metadata.trapi_qEdge_obj.getOutputNode().getID();
+            if (!(nodeID in id_dict)) {
+                id_dict[nodeID] = {};
+                node_dict[nodeID] = record.$edge_metadata.trapi_qEdge_obj.getOutputNode();
+            }
+            debug(`output obj record: ${JSON.stringify(record.$output.obj[0].primaryID)}`);
+            if (!(record.$output.obj[0].primaryID in id_dict[nodeID])) {
+                id_dict[nodeID][record.$output.obj[0].primaryID] = record.$output.obj;
+            }
+        });
+        for (const nodeID in id_dict) {
+            node_dict[nodeID].updateEquivalentIDs(id_dict[nodeID]);
+        }
+    }
 };
