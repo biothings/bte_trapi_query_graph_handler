d1669ca67364f98d4bc209ca279c50d7a655b954
jiwen xin <kevinxin@scripps.edu>
2021-03-17 20:16:05 -0700

    style: :art: style code

--- src/helper.js
+++ src/helper.js
@@ -2,120 +2,120 @@ const crypto = require('crypto');
 const biolink = require('./biolink');
 
 module.exports = class QueryGraphHelper {
-    _generateHash(stringToBeHashed) {
-        return crypto.createHash('md5').update(stringToBeHashed).digest('hex');
-    }
+  _generateHash(stringToBeHashed) {
+    return crypto.createHash('md5').update(stringToBeHashed).digest('hex');
+  }
 
-    _getInputQueryNodeID(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$edge_metadata.trapi_qEdge_obj.getObject().getID()
-            : record.$edge_metadata.trapi_qEdge_obj.getSubject().getID();
-    }
+  _getInputQueryNodeID(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$edge_metadata.trapi_qEdge_obj.getObject().getID()
+      : record.$edge_metadata.trapi_qEdge_obj.getSubject().getID();
+  }
 
-    _getPredicate(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? 'biolink:' + biolink.reverse(record.$edge_metadata.predicate)
-            : 'biolink:' + record.$edge_metadata.predicate;
-    }
+  _getPredicate(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? 'biolink:' + biolink.reverse(record.$edge_metadata.predicate)
+      : 'biolink:' + record.$edge_metadata.predicate;
+  }
 
-    _getOutputQueryNodeID(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$edge_metadata.trapi_qEdge_obj.getSubject().getID()
-            : record.$edge_metadata.trapi_qEdge_obj.getObject().getID();
-    }
+  _getOutputQueryNodeID(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$edge_metadata.trapi_qEdge_obj.getSubject().getID()
+      : record.$edge_metadata.trapi_qEdge_obj.getObject().getID();
+  }
 
-    _getOutputID(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$input.obj[0].primaryID
-            : record.$output.obj[0].primaryID;
-    }
+  _getOutputID(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$input.obj[0].primaryID
+      : record.$output.obj[0].primaryID;
+  }
 
-    _getInputID(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$output.obj[0].primaryID
-            : record.$input.obj[0].primaryID;
-    }
+  _getInputID(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$output.obj[0].primaryID
+      : record.$input.obj[0].primaryID;
+  }
 
-    _getAPI(record) {
-        return record.$edge_metadata.api_name || '';
-    }
+  _getAPI(record) {
+    return record.$edge_metadata.api_name || '';
+  }
 
-    _getSource(record) {
-        return record.$edge_metadata.source || '';
-    }
+  _getSource(record) {
+    return record.$edge_metadata.source || '';
+  }
 
-    _createUniqueEdgeID(record) {
-        const edgeMetaData = [
-            this._getInputID(record),
-            this._getOutputID(record),
-            this._getAPI(record),
-            this._getSource(record),
-        ];
-        // return this._generateHash(edgeMetaData.join('-'));
-        return edgeMetaData.join('-');
-    }
+  _createUniqueEdgeID(record) {
+    const edgeMetaData = [
+      this._getInputID(record),
+      this._getOutputID(record),
+      this._getAPI(record),
+      this._getSource(record),
+    ];
+    // return this._generateHash(edgeMetaData.join('-'));
+    return edgeMetaData.join('-');
+  }
 
-    _getInputCategory(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$output.obj[0].semanticType
-            : record.$input.obj[0].semanticType;
-    }
+  _getInputCategory(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$output.obj[0].semanticType
+      : record.$input.obj[0].semanticType;
+  }
 
-    _getOutputCategory(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$input.obj[0].semanticType
-            : record.$output.obj[0].semanticType;
-    }
+  _getOutputCategory(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$input.obj[0].semanticType
+      : record.$output.obj[0].semanticType;
+  }
 
-    _getOutputLabel(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$input.obj[0].label
-            : record.$output.obj[0].label;
-    }
+  _getOutputLabel(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$input.obj[0].label
+      : record.$output.obj[0].label;
+  }
 
-    _getInputLabel(record) {
-        return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-            ? record.$output.obj[0].label
-            : record.$input.obj[0].label;
-    }
+  _getInputLabel(record) {
+    return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+      ? record.$output.obj[0].label
+      : record.$input.obj[0].label;
+  }
 
-    _getInputEquivalentIds(record) {
-        try {
-            return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-                ? record.$output.obj[0].curies
-                : record.$input.obj[0].curies;
-        } catch (err) {
-            return null;
-        }
+  _getInputEquivalentIds(record) {
+    try {
+      return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+        ? record.$output.obj[0].curies
+        : record.$input.obj[0].curies;
+    } catch (err) {
+      return null;
     }
+  }
 
-    _getInputAttributes(record) {
-        try {
-            return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-                ? record.$output.obj[0].attributes
-                : record.$input.obj[0].attributes;
-        } catch (err) {
-            return null;
-        }
+  _getInputAttributes(record) {
+    try {
+      return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+        ? record.$output.obj[0].attributes
+        : record.$input.obj[0].attributes;
+    } catch (err) {
+      return null;
     }
+  }
 
-    _getOutputEquivalentIds(record) {
-        try {
-            return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-                ? record.$input.obj[0].curies
-                : record.$output.obj[0].curies;
-        } catch (err) {
-            return null;
-        }
+  _getOutputEquivalentIds(record) {
+    try {
+      return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+        ? record.$input.obj[0].curies
+        : record.$output.obj[0].curies;
+    } catch (err) {
+      return null;
     }
+  }
 
-    _getOutputAttributes(record) {
-        try {
-            return record.$edge_metadata.trapi_qEdge_obj.isReversed()
-                ? record.$input.obj[0].attributes
-                : record.$output.obj[0].attributes;
-        } catch (err) {
-            return null;
-        }
+  _getOutputAttributes(record) {
+    try {
+      return record.$edge_metadata.trapi_qEdge_obj.isReversed()
+        ? record.$input.obj[0].attributes
+        : record.$output.obj[0].attributes;
+    } catch (err) {
+      return null;
     }
+  }
 };
