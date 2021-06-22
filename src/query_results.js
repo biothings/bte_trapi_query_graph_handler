97963b6f3379de4f1ed7ff8e5bedbcd303f34271
Anders Riutta <git@andersriutta.com>
2021-06-21 17:43:32 -0700

    fix: WIP for https://github.com/biothings/BioThings_Explorer_TRAPI/issues/164

--- src/query_results.js
+++ src/query_results.js
@@ -5,25 +5,11 @@ const debug = require('debug')('biothings-explorer-trapi:QueryResult');
 module.exports = class QueryResult {
   constructor() {
     this.results = [];
+    this.node2index = {};
   }
 
   getResults() {
     return this.results;
-  }
-
-  _createNodeBindings(record) {
-    return {
-      [helper._getInputQueryNodeID(record)]: [
-        {
-          id: helper._getInputID(record),
-        },
-      ],
-      [helper._getOutputQueryNodeID(record)]: [
-        {
-          id: helper._getOutputID(record),
-        },
-      ],
-    };
   }
 
   _createEdgeBindings(record) {
@@ -38,13 +24,47 @@ module.exports = class QueryResult {
 
   update(queryResult) {
     debug(`Updating query results now!`);
-    queryResult.map((record) => {
-      this.results.push({
-        node_bindings: this._createNodeBindings(record),
-        edge_bindings: this._createEdgeBindings(record),
-        //default score issue #200 - TODO: turn to evaluating module eventually
-        score: '1.0',
+    this.node2index = {};
+    if (Object.keys(this.node2index).length == 0) {
+      queryResult.map((record) => {
+        const outputNodeID = helper._getOutputID(record)
+
+        const node_bindings = {
+          [helper._getInputQueryNodeID(record)]: [
+            {
+              id: helper._getInputID(record),
+            },
+          ],
+          [helper._getOutputQueryNodeID(record)]: [
+            {
+              id: outputNodeID,
+            },
+          ],
+        };
+
+        const result = {
+          node_bindings: node_bindings,
+          edge_bindings: this._createEdgeBindings(record),
+          //default score issue #200 - TODO: turn to evaluating module eventually
+          score: '1.0',
+        };
+        this.results.push(result);
+	this.node2index[outputNodeID] = this.results.length - 1;
       });
-    });
+    } else {
+      queryResult.forEach((record) => {
+        const prevOutputNodeID = helper._getInputID(record);
+        const resultIndex = this.node2index[prevOutputNodeID];
+        const result = this.results[resultIndex];
+
+        const newOutputNodeID = helper._getOutputID(record);
+        result[helper._getOutputQueryNodeID(record)] = [
+          {
+            id: newOutputNodeID,
+  	  },
+        ];
+	this.node2index[newOutputNodeID] = resultIndex;
+      });
+    }
   }
 };
