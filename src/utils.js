679001f9e5e0aa7c3ded7b229f3545d051a836fc
Anders Riutta <git@andersriutta.com>
2021-08-27 13:51:38 -0700

    Fix: QueryResult works w/ generalized handling. Close #164 and #254.

--- src/utils.js
+++ src/utils.js
@@ -15,3 +15,37 @@ exports.removeBioLinkPrefix = (input) => {
   }
   return input;
 };
+
+// This gets the intersection of two sets.
+// Lodash _.intersection gets the intersection of two arrays.
+// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+const intersection = (setA, setB) => {
+  let resultSet = new Set()
+  for (let elem of setB) {
+    if (setA.has(elem)) {
+      resultSet.add(elem)
+    }
+  }
+  return resultSet
+}
+exports.intersection = intersection;
+
+// see https://stackoverflow.com/a/29585704
+const cartesian = (a) => { // a = array of arrays
+  var i, j, l, m, a1, o = [];
+  if (!a || a.length == 0) return a;
+
+  a1 = a.splice(0, 1)[0]; // the first array of a
+  a = cartesian(a);
+  for (i = 0, l = a1.length; i < l; i++) {
+    if (a && a.length) {
+      for (j = 0, m = a.length; j < m; j++) {
+        o.push([a1[i]].concat(a[j]));
+      }
+    } else {
+      o.push([a1[i]]);
+    }
+  }
+  return o;
+}
+exports.cartesian = cartesian;
