ef30cea5a271351214151e09ba349c16eb2b7342
Colleen Xu <colleenhxu@gmail.com>
2021-10-29 15:40:17 -0700

    remove extra pending apis

--- src/config.js
+++ src/config.js
@@ -10,57 +10,6 @@ exports.API_MAX_ID_LIST = [
     name: 'Text Mining Targeted Association API',
     max: 100,
   },
-  // other pending apis that use POST (batch-query IDs)
-  {
-    id: 'e3edd325c76f2992a111b43a907a4870',
-    name: 'BioThings DGIdb API',
-    max: 100,
-  },
-  {
-    id: '32f36164fabed5d3abe6c2fd899c9418',
-    name: 'BioThings iDISK API',
-    max: 100,
-  },
-  {
-    id: 'a7f784626a426d054885a5f33f17d3f8',
-    name: 'DISEASES API',
-    max: 100,
-  },
-  {
-    id: '1f47552dabd67351d4c625adb0a10d00',
-    name: 'EBIgene2phenotype API',
-    max: 100,
-  },
-  {
-    id: 'cc857d5b7c8b7609b5bbb38ff990bfff',
-    name: 'Gene Ontology Biological Process API',
-    max: 100,
-  },
-  {
-    id: 'f339b28426e7bf72028f60feefcd7465',
-    name: 'Gene Ontology Cellular Component API',
-    max: 100,
-  },
-  {
-    id: '34bad236d77bea0a0ee6c6cba5be54a6',
-    name: 'Gene Ontology Molecular Activity API',
-    max: 100,
-  },
-  {
-    id: 'a5b0ec6bfde5008984d4b6cde402d61f',
-    name: 'Human Phenotype Ontology API',
-    max: 100,
-  },
-  {
-    id: '77ed27f111262d0289ed4f4071faa619',
-    name: 'MGIgene2phenotype API',
-    max: 100,
-  },
-  {
-    id: 'ec6d76016ef40f284359d17fbf78df20',
-    name: 'UBERON Ontology API',
-    max: 100,
-  }
 ];
 
 //max node IDs an edge with no other IDs can have
