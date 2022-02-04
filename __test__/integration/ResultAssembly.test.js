e77b0bf470c1b4bdae2ff5a14a682c88254bf3b2
Marco Cano <artofmarco@gmail.com>
2022-02-04 15:58:01 -0800

    dev: :spakles: synth record creation class

--- __test__/integration/ResultAssembly.test.js
+++ __test__/integration/ResultAssembly.test.js
@@ -7,21 +7,6 @@ class ResultMaker{
     constructor(query, number_of_results) {
         this.query = query;
         this.number_of_results = number_of_results || 1;
-        this.result_template = {
-            '$edge_metadata': {
-                'trapi_qEdge_obj': null,
-                'predicate': null,
-                'api_name': null,
-            },
-            '$input': {
-                'original': null,
-                'obj': [{'primaryID': null}]
-            },
-            '$output': {
-                'original': null,
-                'obj': [{'primaryID': null}]
-            },
-        };
         this.results = {};
         this.createResults();
     }
@@ -45,9 +30,24 @@ class ResultMaker{
             this.results[edgeID]['connected_to'] = this.query.message.query_graph.edges[edgeID]['$connected_to'];
             this.results[edgeID]['records'] = [];
 
-            //create results equal to number_of_results
-            for (let i = 0; i < this.number_of_results.length; i++) {
-                let res = {...this.result_template};
+            //create results per edge equal to number_of_results
+            for (let i = 0; i < this.number_of_results; i++) {
+                //result template
+                let res = {
+                    '$edge_metadata': {
+                        'trapi_qEdge_obj': null,
+                        'predicate': null,
+                        'api_name': null,
+                    },
+                    '$input': {
+                        'original': null,
+                        'obj': [{'primaryID': null}]
+                    },
+                    '$output': {
+                        'original': null,
+                        'obj': [{'primaryID': null}]
+                    },
+                };
                 const input = new QNode(inputNodeID, inputNodeInfo);
                 const output = new QNode(outputNodeID, outputNodeInfo);
                 const trapi_edge = new QEdge(edgeID, { 'subject': input, 'object': output });
@@ -56,12 +56,27 @@ class ResultMaker{
                 res.$edge_metadata.trapi_qEdge_obj = trapi_edge;
                 res.$edge_metadata.api_name = this.generateAPIName();
                 res.$edge_metadata.predicate= this.generatePredicate();
-                // input
-                res.$edge_metadata.$input.original = inputNodeInfo?.ids?.[0] || this.generateID();
-                res.$edge_metadata.$input.obj[0].primaryID = inputNodeInfo?.ids?.[0] || this.generateID();
-                // output
-                res.$edge_metadata.$output.original = outputNodeInfo?.ids?.[0] || this.generateID();
-                res.$edge_metadata.$output.obj[0].primaryID = outputNodeInfo?.ids?.[0] || this.generateID();
+                
+
+                if (inputNodeInfo?.ids) {
+                    let randomID = inputNodeInfo?.ids[Math.floor(Math.random() * inputNodeInfo?.ids.length)];
+                    // input
+                    res['$input'].original = randomID;
+                    res['$input'].obj[0].primaryID = randomID;
+                }else{
+                    res['$input'].original = this.generateID();
+                    res['$input'].obj[0].primaryID = this.generateID();
+                }
+
+                if (outputNodeInfo?.ids) {
+                    let randomID = outputNodeInfo?.ids[Math.floor(Math.random() * outputNodeInfo?.ids.length)];
+                    // output
+                    res['$output'].original = randomID;
+                    res['$output'].obj[0].primaryID = randomID;
+                }else{
+                    res['$output'].original = this.generateID();
+                    res['$output'].obj[0].primaryID = this.generateID();
+                }
                 //generated result for current edge
                 this.results[edgeID]['records'].push(res);
             }
@@ -74,67 +89,70 @@ class ResultMaker{
     }
 
     generateID(){
-        return "⚽️-" + Math.floor(Math.random() * 40) + 1;
+        return "ID-" + Math.floor(Math.random() * 40) + 1;
     }
 
-    generateID(){
+    generatePredicate(){
         return "predicate-" + Math.floor(Math.random() * 20) + 1;
     }
 
-    get generatedResults() {
+    get getOrganizedResults() {
         return this.results;
     }
 }
 
 describe('Testing QueryResults', () => {
-    describe('query graph: 🍊-💧->🌸', () => {
+    describe('One hop', () => {
 
-        let query = {
-            "message": {
-                "query_graph": {
-                    "nodes": {
-                        "n0": {
-                            "categories": ["🍊"],
-                            "ids": [ "A"],
-                            "is_set": false
+        test('2 result 4 edge bindings', () => {
+            let query = {
+                "message": {
+                    "query_graph": {
+                        "nodes": {
+                            "n0": {
+                                "categories": ["🍊"],
+                                "ids": [ "A"],
+                                "is_set": false
+                            },
+                            "n1": {
+                                "categories": ["🌸"],
+                                "ids": ["B", "C"],
+                                "is_set": false
+                            }
                         },
-                        "n1": {
-                            "categories": ["🌸"],
-                            "ids": ["B", "C"],
-                            "is_set": false
-                        }
-                    },
-                    "edges": {
-                        "e0": {
-                            "subject": "n0",
-                            "object": "n1",
-                            "predicates": ["💧"],
-                            //to make this easier include this new property
-                            "$connected_to": []
+                        "edges": {
+                            "e0": {
+                                "subject": "n0",
+                                "object": "n1",
+                                "predicates": ["💧"],
+                                //to make this easier include this new property
+                                "$connected_to": []
+                            }
                         }
                     }
                 }
             }
-        }
+    
+            const rm =  new ResultMaker(query, 4);
+            const organized_results = rm.getOrganizedResults;
+    
+            const queryResult = new QueryResult();
+    
+            queryResult.update(organized_results);
+    
+            const results = queryResult.getResults();
+    
+            expect(results.length).toEqual(2);
+    
+            expect(Object.keys(results[0].node_bindings).length).toEqual(2);
+            expect(results[0].node_bindings).toHaveProperty('n0');
+            expect(results[0].node_bindings).toHaveProperty('n1');
+    
+            expect(Object.keys(results[0].edge_bindings).length).toEqual(1);
+            expect(results[0].edge_bindings).toHaveProperty('e0');
+    
+            expect(results[0]).toHaveProperty('score');
+        });
 
-        const manager =  new ResultMaker(query, 4);
-        const organized_results = manager.generatedResults;
-
-        const queryResult = new QueryResult();
-
-        queryResult.update(organized_results);
-
-        const results = queryResult.getResults();
-
-        expect(results.length).toEqual(4);
-
-        expect(Object.keys(results[0].node_bindings).length).toEqual(3);
-        expect(results[0].node_bindings).toHaveProperty('n0');
-        expect(results[0].node_bindings).toHaveProperty('n1');
-
-        expect(Object.keys(results[0].edge_bindings).length).toEqual(1);
-        expect(results[0].edge_bindings).toHaveProperty('e0');
-
-        expect(results[0]).toHaveProperty('score');
     });
 });
