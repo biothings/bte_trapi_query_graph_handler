a08ab4c5f26b93b288a178784f6ebe052b3acb84
tokebe <43009413+tokebe@users.noreply.github.com>
2022-03-21 16:29:26 -0400

    fix: reduce redis lock timeout to 5min

--- src/redis-client.js
+++ src/redis-client.js
@@ -47,7 +47,7 @@ const redisClient =
         hgetallAsync: timeoutFunc(promisify(client.hgetall).bind(client)),
         expireAsync: timeoutFunc(promisify(client.expire).bind(client)),
         delAsync: timeoutFunc(promisify(client.del).bind(client)),
-        lock: timeoutFunc(promisify(redisLock(client)), 20 * 60 * 1000),
+        lock: timeoutFunc(promisify(redisLock(client)), 5 * 60000),
         hmsetAsync: timeoutFunc(promisify(client.hmset).bind(client)),
         keysAsync: timeoutFunc(promisify(client.keys).bind(client)),
         existsAsync: timeoutFunc(promisify(client.exists).bind(client)),
