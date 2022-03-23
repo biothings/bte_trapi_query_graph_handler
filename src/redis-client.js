2765de5f308d98a7b9d85cb6b13151cbb3cc5930
tokebe <43009413+tokebe@users.noreply.github.com>
2022-03-23 14:53:11 -0400

    feat: redis lolwut for connection testing

--- src/redis-client.js
+++ src/redis-client.js
@@ -51,7 +51,7 @@ const redisClient =
         hmsetAsync: timeoutFunc(promisify(client.hmset).bind(client)),
         keysAsync: timeoutFunc(promisify(client.keys).bind(client)),
         existsAsync: timeoutFunc(promisify(client.exists).bind(client)),
+        lolwutAsync: timeoutFunc(promisify(client.lolwut).bind(client), 1000) // for testing
       }
     : {};
-
 module.exports = redisClient;
