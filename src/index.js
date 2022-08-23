c030737cc447a3159ec33ceeec2284d018108195
tokebe <43009413+tokebe@users.noreply.github.com>
2022-08-23 15:07:18 -0400

    Revert "fix workflow id"
    This reverts commit b75031166783e1e02b148a3a5cbb6105ca8664d9.

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ exports.TRAPIQueryHandler = class TRAPIQueryHandler {
 
   getResponse() {
     return {
-      workflow: [{ id: 'lookup_and_score' }],
+      workflow: [{ id: 'lookup' }],
       message: {
         query_graph: this.queryGraph,
         knowledge_graph: this.knowledgeGraph.kg,
@@ -387,7 +387,7 @@ exports.TRAPIQueryHandler = class TRAPIQueryHandler {
     });
 
     const combinedResponse = {
-      workflow: [{ id: 'lookup_and_score' }],
+      workflow: [{ id: 'lookup' }],
       message: {
         query_graph: {},
         knowledge_graph: {
