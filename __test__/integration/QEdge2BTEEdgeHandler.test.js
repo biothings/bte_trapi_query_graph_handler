c475e0aff84565254da8a10a75de5ef418690358
tokebe <43009413+tokebe@users.noreply.github.com>
2022-10-21 14:55:01 -0400

    style: formatting

--- __test__/integration/QEdge2BTEEdgeHandler.test.js
+++ __test__/integration/QEdge2BTEEdgeHandler.test.js
@@ -1,68 +1,70 @@
-const QNode = require("../../src/query_node");
-const QEdge = require("../../src/query_edge");
-const NodeUpdateHandler = require("../../src/update_nodes");
+const QNode = require('../../src/query_node');
+const QEdge = require('../../src/query_edge');
+const NodeUpdateHandler = require('../../src/update_nodes');
 
-describe("Testing NodeUpdateHandler Module", () => {
-    const gene_node1 = new QNode({ id: "n1", categories: ["Gene"], ids: ["NCBIGene:1017"] }) ;
-    const node1_equivalent_ids = {
-        "NCBIGene:1017": {
-            db_ids: {
-                NCBIGene: ["1017"],
-                SYMBOL: ['CDK2']
-            }
-        }
-    }
+describe('Testing NodeUpdateHandler Module', () => {
+  const gene_node1 = new QNode({ id: 'n1', categories: ['Gene'], ids: ['NCBIGene:1017'] });
+  const node1_equivalent_ids = {
+    'NCBIGene:1017': {
+      db_ids: {
+        NCBIGene: ['1017'],
+        SYMBOL: ['CDK2'],
+      },
+    },
+  };
 
-    const gene_node2 = new QNode({ id: "n2", categories: ["Gene"], ids: ["NCBIGene:1017", "NCBIGene:1018"] }) ;
-    const gene_node1_with_id_annotated = new QNode({ id: "n1", categories: ["Gene"], ids: ["NCBIGene:1017"] }) ;
-    gene_node1_with_id_annotated.setEquivalentIDs(node1_equivalent_ids);
-    //gene_node2.setEquivalentIDs(node2_equivalent_ids);
-    const chemical_node1 = new QNode({ id: "n3", categories:[ "SmallMolecule"] }) ;
-    const edge1 = new QEdge({ id: "e01", subject: gene_node1, object: chemical_node1 });
-    const edge2 = new QEdge({ id: "e02", subject: gene_node1_with_id_annotated, object: chemical_node1 });
-    const edge3 = new QEdge({ id: 'e04', subject: gene_node2, object: chemical_node1 });
-    const edge4 = new QEdge({ id: 'e05', object: gene_node2, subject: chemical_node1 });
+  const gene_node2 = new QNode({ id: 'n2', categories: ['Gene'], ids: ['NCBIGene:1017', 'NCBIGene:1018'] });
+  const gene_node1_with_id_annotated = new QNode({ id: 'n1', categories: ['Gene'], ids: ['NCBIGene:1017'] });
+  gene_node1_with_id_annotated.setEquivalentIDs(node1_equivalent_ids);
+  //gene_node2.setEquivalentIDs(node2_equivalent_ids);
+  const chemical_node1 = new QNode({ id: 'n3', categories: ['SmallMolecule'] });
+  const edge1 = new QEdge({ id: 'e01', subject: gene_node1, object: chemical_node1 });
+  const edge2 = new QEdge({ id: 'e02', subject: gene_node1_with_id_annotated, object: chemical_node1 });
+  const edge3 = new QEdge({ id: 'e04', subject: gene_node2, object: chemical_node1 });
+  const edge4 = new QEdge({ id: 'e05', object: gene_node2, subject: chemical_node1 });
 
-    describe("Testing _getCuries function", () => {
-        test("test edge with one curie input return an array of one", () => {
-            const nodeUpdater = new NodeUpdateHandler([edge1]);
-            const res = nodeUpdater._getCuries([edge1]);
-            expect(res).toHaveProperty("Gene", ["NCBIGene:1017"]);
-        })
+  describe('Testing _getCuries function', () => {
+    test('test edge with one curie input return an array of one', () => {
+      const nodeUpdater = new NodeUpdateHandler([edge1]);
+      const res = nodeUpdater._getCuries([edge1]);
+      expect(res).toHaveProperty('Gene', ['NCBIGene:1017']);
+    });
 
-        test("test edge with multiple curie input return an array with multiple items", () => {
-            const nodeUpdater = new NodeUpdateHandler([edge3]);
-            const res = nodeUpdater._getCuries([edge3]);
-            expect(res.Gene.length).toEqual(2);
-        })
+    test('test edge with multiple curie input return an array with multiple items', () => {
+      const nodeUpdater = new NodeUpdateHandler([edge3]);
+      const res = nodeUpdater._getCuries([edge3]);
+      expect(res.Gene.length).toEqual(2);
+    });
 
-        test("test edge with input node annotated should return an empty array", () => {
-            const nodeUpdater = new NodeUpdateHandler([edge2]);
-            const res = nodeUpdater._getCuries([edge2]);
-            expect(res).toEqual({});
-        })
+    test('test edge with input node annotated should return an empty array', () => {
+      const nodeUpdater = new NodeUpdateHandler([edge2]);
+      const res = nodeUpdater._getCuries([edge2]);
+      expect(res).toEqual({});
+    });
 
-        test("test edge with input on object end should be handled", () => {
-            const nodeUpdater = new NodeUpdateHandler([edge4]);
-            const res = nodeUpdater._getCuries([edge4]);
-            expect(res.Gene.length).toEqual(2);
-        })
-    })
+    test('test edge with input on object end should be handled', () => {
+      const nodeUpdater = new NodeUpdateHandler([edge4]);
+      const res = nodeUpdater._getCuries([edge4]);
+      expect(res.Gene.length).toEqual(2);
+    });
+  });
 
-    describe("Testing _getEquivalentIDs function", () => {
-        test("test edge with one curie input return an object with one key", async () => {
-            const nodeUpdater = new NodeUpdateHandler([edge1]);
-            const res = await nodeUpdater._getEquivalentIDs({ "Gene": ["NCBIGene:1017"] })
-            expect(res).toHaveProperty("NCBIGene:1017");
-        })
+  describe('Testing _getEquivalentIDs function', () => {
+    test('test edge with one curie input return an object with one key', async () => {
+      const nodeUpdater = new NodeUpdateHandler([edge1]);
+      const res = await nodeUpdater._getEquivalentIDs({ Gene: ['NCBIGene:1017'] });
+      expect(res).toHaveProperty('NCBIGene:1017');
+    });
 
-        test("test edge with multiple curie input return an object with multiple key", async () => {
-            const nodeUpdater = new NodeUpdateHandler([edge1]);
-            const res = await nodeUpdater._getEquivalentIDs({ "Gene": ["NCBIGene:1017", "NCBIGene:1018"], "SmallMolecule": ["PUBCHEM:5070"] })
-            expect(res).toHaveProperty("NCBIGene:1017");
-            expect(res).toHaveProperty("NCBIGene:1018");
-            expect(res).toHaveProperty("PUBCHEM:5070");
-        })
-    })
-
-})
+    test('test edge with multiple curie input return an object with multiple key', async () => {
+      const nodeUpdater = new NodeUpdateHandler([edge1]);
+      const res = await nodeUpdater._getEquivalentIDs({
+        Gene: ['NCBIGene:1017', 'NCBIGene:1018'],
+        SmallMolecule: ['PUBCHEM:5070'],
+      });
+      expect(res).toHaveProperty('NCBIGene:1017');
+      expect(res).toHaveProperty('NCBIGene:1018');
+      expect(res).toHaveProperty('PUBCHEM:5070');
+    });
+  });
+});
--- __test__/integration/QueryNode.test.js
+++ __test__/integration/QueryNode.test.js
@@ -1,165 +1,165 @@
-const QNode = require("../../src/query_node");
+const QNode = require('../../src/query_node');
 
-describe("Testing QueryNode Module", () => {
-    const node1_equivalent_ids = {
-        "NCBIGene:1017": {
-            db_ids: {
-                NCBIGene: ["1017"],
-                SYMBOL: ['CDK2']
-            }
-        }
-    }
+describe('Testing QueryNode Module', () => {
+  const node1_equivalent_ids = {
+    'NCBIGene:1017': {
+      db_ids: {
+        NCBIGene: ['1017'],
+        SYMBOL: ['CDK2'],
+      },
+    },
+  };
 
-    describe("Testing hasInput function", () => {
-        test("test node without curies specified should return false", () => {
-            const gene_node = new QNode({ id: "n1", categories: ["Gene"] }) ;
-            const res = gene_node.hasInput();
-            expect(res).toBeFalsy();
-        })
+  describe('Testing hasInput function', () => {
+    test('test node without curies specified should return false', () => {
+      const gene_node = new QNode({ id: 'n1', categories: ['Gene'] });
+      const res = gene_node.hasInput();
+      expect(res).toBeFalsy();
+    });
 
-        test("test node with curies specified should return true", () => {
-            const gene_node = new QNode({ id: "n1", categories: ["Gene"], ids: ["NCBIGene:1017"] }) ;
-            const res = gene_node.hasInput();
-            expect(res).toBeTruthy();
-        })
-    })
+    test('test node with curies specified should return true', () => {
+      const gene_node = new QNode({ id: 'n1', categories: ['Gene'], ids: ['NCBIGene:1017'] });
+      const res = gene_node.hasInput();
+      expect(res).toBeTruthy();
+    });
+  });
 
-    describe("Test hasEquivalentIDs function", () => {
-        test("test node with equivalent identifiers set should return true", () => {
-            const gene_node = new QNode({ id: "n1", categories: ["Gene"] }) ;
-            gene_node.setEquivalentIDs(node1_equivalent_ids);
-            const res = gene_node.hasEquivalentIDs();
-            expect(res).toBeTruthy();
-        });
+  describe('Test hasEquivalentIDs function', () => {
+    test('test node with equivalent identifiers set should return true', () => {
+      const gene_node = new QNode({ id: 'n1', categories: ['Gene'] });
+      gene_node.setEquivalentIDs(node1_equivalent_ids);
+      const res = gene_node.hasEquivalentIDs();
+      expect(res).toBeTruthy();
+    });
 
-        test("test node with equivalent identifiers not set should return false", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            const res = gene_node.hasEquivalentIDs();
-            expect(res).toBeFalsy();
-        })
-    })
+    test('test node with equivalent identifiers not set should return false', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      const res = gene_node.hasEquivalentIDs();
+      expect(res).toBeFalsy();
+    });
+  });
 
-    describe("Test getEntities", () => {
-        test("If equivalent ids are empty, should return an empty array", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            gene_node.equivalentIDs = {};
-            expect(gene_node.getEntities()).toEqual([]);
-        })
+  describe('Test getEntities', () => {
+    test('If equivalent ids are empty, should return an empty array', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      gene_node.equivalentIDs = {};
+      expect(gene_node.getEntities()).toEqual([]);
+    });
 
-        test("If equivalent ids are not empty, should return an array of bioentities", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            gene_node.equivalentIDs = {
-                "A": [
-                    {
-                        "a": "b"
-                    },
-                    {
-                        "c": "d"
-                    }
-                ],
-                "B": [
-                    {
-                        "e": "f"
-                    }
-                ]
-            };
-            expect(gene_node.getEntities()).toEqual([
-                {
-                    "a": "b"
-                },
-                {
-                    "c": "d"
-                },
-                {
-                    "e": "f"
-                }
-            ]);
-        })
-    })
+    test('If equivalent ids are not empty, should return an array of bioentities', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      gene_node.equivalentIDs = {
+        A: [
+          {
+            a: 'b',
+          },
+          {
+            c: 'd',
+          },
+        ],
+        B: [
+          {
+            e: 'f',
+          },
+        ],
+      };
+      expect(gene_node.getEntities()).toEqual([
+        {
+          a: 'b',
+        },
+        {
+          c: 'd',
+        },
+        {
+          e: 'f',
+        },
+      ]);
+    });
+  });
 
-    describe("Test getPrimaryIDs", () => {
-        test("If equivalent ids are empty, should return an empty array", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            gene_node.equivalentIDs = {};
-            expect(gene_node.getPrimaryIDs()).toEqual([]);
-        })
+  describe('Test getPrimaryIDs', () => {
+    test('If equivalent ids are empty, should return an empty array', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      gene_node.equivalentIDs = {};
+      expect(gene_node.getPrimaryIDs()).toEqual([]);
+    });
 
-        test("If equivalent ids are not empty, should return an array of primaryIDs", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            gene_node.equivalentIDs = {
-                "A": [
-                    {
-                        "primaryID": "b"
-                    },
-                    {
-                        "primaryID": "c"
-                    }
-                ],
-                "B": [
-                    {
-                        "primaryID": "d"
-                    }
-                ]
-            };
-            expect(gene_node.getPrimaryIDs()).toEqual(["b", "c", "d"])
-        })
-    })
+    test('If equivalent ids are not empty, should return an array of primaryIDs', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      gene_node.equivalentIDs = {
+        A: [
+          {
+            primaryID: 'b',
+          },
+          {
+            primaryID: 'c',
+          },
+        ],
+        B: [
+          {
+            primaryID: 'd',
+          },
+        ],
+      };
+      expect(gene_node.getPrimaryIDs()).toEqual(['b', 'c', 'd']);
+    });
+  });
 
-    describe("Test updateEquivalentIDs", () => {
-        test("If equivalent ids does not exist, should set it with the input", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            gene_node.updateEquivalentIDs({ "a": "b" })
-            expect(gene_node.equivalentIDs).toEqual({ "a": "b" });
-        })
+  describe('Test updateEquivalentIDs', () => {
+    test('If equivalent ids does not exist, should set it with the input', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      gene_node.updateEquivalentIDs({ a: 'b' });
+      expect(gene_node.equivalentIDs).toEqual({ a: 'b' });
+    });
 
-        test("If equivalent ids are not empty, should update the equivalent ids", () => {
-            const gene_node = new QNode({ id: "n1", categories: "Gene" }) ;
-            gene_node.equivalentIDs = { "a": "b", "c": "d" };
-            gene_node.updateEquivalentIDs({ "e": "f" })
-            expect(gene_node.getEquivalentIDs()).toEqual({ "a": "b", "c": "d", "e": "f" })
-        })
-    })
+    test('If equivalent ids are not empty, should update the equivalent ids', () => {
+      const gene_node = new QNode({ id: 'n1', categories: 'Gene' });
+      gene_node.equivalentIDs = { a: 'b', c: 'd' };
+      gene_node.updateEquivalentIDs({ e: 'f' });
+      expect(gene_node.getEquivalentIDs()).toEqual({ a: 'b', c: 'd', e: 'f' });
+    });
+  });
 
-    describe("Test getCategories function", () => {
-        test("If equivalent ids are empty, return itself and its descendants", () => {
-            const node = new QNode({ id: "n1", categories: "DiseaseOrPhenotypicFeature" }) ;
-            expect(node.getCategories()).toContain("Disease");
-            expect(node.getCategories()).toContain("PhenotypicFeature");
-            expect(node.getCategories()).toContain("DiseaseOrPhenotypicFeature");
-        })
+  describe('Test getCategories function', () => {
+    test('If equivalent ids are empty, return itself and its descendants', () => {
+      const node = new QNode({ id: 'n1', categories: 'DiseaseOrPhenotypicFeature' });
+      expect(node.getCategories()).toContain('Disease');
+      expect(node.getCategories()).toContain('PhenotypicFeature');
+      expect(node.getCategories()).toContain('DiseaseOrPhenotypicFeature');
+    });
 
-        test("If equivalent ids are empty, return itself and its descendants using NamedThing as example", () => {
-            const node = new QNode({ id: "n1", categories: "NamedThing" }) ;
-            expect(node.getCategories()).toContain("Disease");
-            expect(node.getCategories()).toContain("PhenotypicFeature");
-            expect(node.getCategories()).toContain("DiseaseOrPhenotypicFeature");
-            expect(node.getCategories()).toContain("Gene");
-            expect(node.getCategories()).toContain("NamedThing");
-        })
+    test('If equivalent ids are empty, return itself and its descendants using NamedThing as example', () => {
+      const node = new QNode({ id: 'n1', categories: 'NamedThing' });
+      expect(node.getCategories()).toContain('Disease');
+      expect(node.getCategories()).toContain('PhenotypicFeature');
+      expect(node.getCategories()).toContain('DiseaseOrPhenotypicFeature');
+      expect(node.getCategories()).toContain('Gene');
+      expect(node.getCategories()).toContain('NamedThing');
+    });
 
-        test("If equivalent ids are empty, return itself and its descendants using Gene as example", () => {
-            const node = new QNode({ id: "n1", categories: "Gene" }) ;
-            expect(node.getCategories()).toEqual(["Gene"])
-        })
+    test('If equivalent ids are empty, return itself and its descendants using Gene as example', () => {
+      const node = new QNode({ id: 'n1', categories: 'Gene' });
+      expect(node.getCategories()).toEqual(['Gene']);
+    });
 
-        test("If equivalent ids are not empty, return all semantic types defined in the entity", () => {
-            const node = new QNode({ id: "n1", categories: "Gene" }) ;
-            node.equivalentIDs = {
-                "A": [
-                    {
-                        "semanticTypes": ["m", "n"]
-                    },
-                    {
-                        "semanticTypes": ["p", "q"]
-                    }
-                ],
-                "B": [
-                    {
-                        "semanticTypes": ["x", "y"]
-                    }
-                ]
-            };
-            expect(node.getCategories()).toEqual(["m", "n", "p", "q", "x", "y"])
-        })
-    })
-})
+    test('If equivalent ids are not empty, return all semantic types defined in the entity', () => {
+      const node = new QNode({ id: 'n1', categories: 'Gene' });
+      node.equivalentIDs = {
+        A: [
+          {
+            semanticTypes: ['m', 'n'],
+          },
+          {
+            semanticTypes: ['p', 'q'],
+          },
+        ],
+        B: [
+          {
+            semanticTypes: ['x', 'y'],
+          },
+        ],
+      };
+      expect(node.getCategories()).toEqual(['m', 'n', 'p', 'q', 'x', 'y']);
+    });
+  });
+});
