48ccfa6389a5edec8ea51752e17a8e35f2a52c9e
tokebe <43009413+tokebe@users.noreply.github.com>
2023-04-25 15:46:36 -0700

    style: format

--- src/results_assembly/score.js
+++ src/results_assembly/score.js
@@ -8,29 +8,26 @@ async function query(queryPairs) {
   const url = 'https://biothings.ncats.io/semmeddb/query/ngd';
   const batchSize = 1000;
 
-  debug("Querying", queryPairs.length, "combos.");
+  debug('Querying', queryPairs.length, 'combos.');
 
   let chunked_input = _.chunk(queryPairs, batchSize);
   try {
     let axios_queries = chunked_input.map((input) => {
-      return axios.post(
-        url,
-        {
-          umls: input,
-          expand: "both"
-        },
-      );
+      return axios.post(url, {
+        umls: input,
+        expand: 'both',
+      });
     });
     //convert res array into single object with all curies
     let res = await Promise.all(axios_queries);
-    res = res.map(r => r.data.filter(combo => Number.isFinite(combo.ngd))).flat(); // get numerical scores and flatten array
+    res = res.map((r) => r.data.filter((combo) => Number.isFinite(combo.ngd))).flat(); // get numerical scores and flatten array
     return res;
   } catch (err) {
-    debug("Failed to query for scores: ", err);
+    debug('Failed to query for scores: ', err);
   }
 }
 
-async function getScores (recordsByQEdgeID) {
+async function getScores(recordsByQEdgeID) {
   let pairs = {};
 
   let combosWithoutIDs = 0;
@@ -46,7 +43,7 @@ async function getScores (recordsByQEdgeID) {
         }
         outputUMLS?.forEach((output_umls) => {
           pairs[input_umls].add(output_umls);
-        })
+        });
       });
 
       if (inputUMLS.length == 0 || outputUMLS.length == 0) {
@@ -56,14 +53,15 @@ async function getScores (recordsByQEdgeID) {
     });
   });
 
-  let queries = Object.keys(pairs).map((inputUMLS) => {
-    return [...pairs[inputUMLS]].map((outputUMLS) => ([inputUMLS, outputUMLS]));
-  }).flat();
-
+  let queries = Object.keys(pairs)
+    .map((inputUMLS) => {
+      return [...pairs[inputUMLS]].map((outputUMLS) => [inputUMLS, outputUMLS]);
+    })
+    .flat();
 
   let results = await query(queries);
 
-  debug("Combos no UMLS ID: ", combosWithoutIDs);
+  debug('Combos no UMLS ID: ', combosWithoutIDs);
   return results || []; // in case results is undefined, avoid TypeErrors
 }
 
@@ -87,15 +85,15 @@ async function getScores (recordsByQEdgeID) {
 // }
 
 // sigmoid function scaled from 0 to 1
-function scaled_sigmoid (input) {
-  const tuned_input = Math.max(input, 0)/tuning_param;
-  const sigmoid = 1/(1 + Math.exp(-tuned_input))
+function scaled_sigmoid(input) {
+  const tuned_input = Math.max(input, 0) / tuning_param;
+  const sigmoid = 1 / (1 + Math.exp(-tuned_input));
   return sigmoid * 2 - 1;
 }
 
-function reverse_scaled_sigmoid (score) {
-  const unscaled_sigmoid = (score + 1)/2;
-  const tuned_input = -Math.log(1/unscaled_sigmoid - 1);
+function reverse_scaled_sigmoid(score) {
+  const unscaled_sigmoid = (score + 1) / 2;
+  const tuned_input = -Math.log(1 / unscaled_sigmoid - 1);
   return tuned_input * tuning_param;
 }
 
@@ -106,12 +104,15 @@ function calculateScore(comboInfo, scoreCombos) {
   Object.keys(comboInfo).forEach((edgeKey) => {
     score += 0.05 * comboInfo[edgeKey].recordHashes.size;
     for (const combo of scoreCombos) {
-      if (comboInfo[edgeKey].inputUMLS?.includes(combo.umls[0]) && comboInfo[edgeKey].outputUMLS?.includes(combo.umls[1])) {
-        score += 1/combo.ngd;
+      if (
+        comboInfo[edgeKey].inputUMLS?.includes(combo.umls[0]) &&
+        comboInfo[edgeKey].outputUMLS?.includes(combo.umls[1])
+      ) {
+        score += 1 / combo.ngd;
         scoredByNGD = true;
       }
     }
-  })
+  });
 
   return { score: scaled_sigmoid(score), scoredByNGD };
 }
