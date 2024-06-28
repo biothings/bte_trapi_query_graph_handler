69258438fc3a09fa85cd3926a0d7ae8781242042
tokebe <43009413+tokebe@users.noreply.github.com>
2024-06-28 16:38:17 -0400

    feat: change scoring sigmoid function to arctan
    Should give more granularity at the high end of scores without significantly lowering mid- and lower-end scores

--- src/results_assembly/score.ts
+++ src/results_assembly/score.ts
@@ -8,7 +8,7 @@ import _ from 'lodash';
 import { ConsolidatedSolutionRecord, RecordsByQEdgeID } from './query_results';
 import { Telemetry } from '@biothings-explorer/utils';
 
-const tuning_param = 2.0;
+const tuning_param = 1.8;
 
 const record_weight = 1.0;
 const text_mined_record_weight = 0.5;
@@ -109,8 +109,7 @@ export async function getScores(recordsByQEdgeID: RecordsByQEdgeID): Promise<Sco
 // sigmoid function scaled from 0 to 1
 export function scaled_sigmoid(input: number): number {
   const tuned_input = Math.max(input, 0) / tuning_param;
-  const sigmoid = 1 / (1 + Math.exp(-tuned_input));
-  return sigmoid * 2 - 1;
+  return (2 / Math.PI) * Math.atan(tuned_input);
 }
 
 export function inverse_scaled_sigmoid(input: number): number {
